d353b2dd756fea4508acf292e3d41b9a386a2dbf
Andreï V. Kostyrka <fifis.himik@gmail.com>
2024-06-12 04:19:38 +0200

    New unit tests for kernel and SEL functions

--- tests/testthat/test-ctracelr.R
+++ tests/testthat/test-ctracelr.R
@@ -4,5 +4,9 @@ test_that("ctracelr constructs a correct path", {
   trc <- ctracelr(xy, mu0 = c(0.5, 0.5), mu1 = c(1.5, 1.5), N = 100)
   expect_equal(nrow(trc), 101)
   expect_true(all(trc[, "conv"] == 1))
+<<<<<<< HEAD
   expect_true(all(trc[, "gnorm"] < 1e-7))
+=======
+  expect_lt(max(trc[, "gnorm"]), 1e-6)
+>>>>>>> aa077fb (New unit tests for kernel and SEL functions)
 })
